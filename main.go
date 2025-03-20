21314b663b42a9fd095ee56960b4231dbb9dca18
mathisdev7 <999frostz@gmail.com>
2025-03-20 17:16:50 +0100

    feat(role): add role position route

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/mathisdev7/licensy-dashboard-backend/config"
 	"github.com/mathisdev7/licensy-dashboard-backend/handlers"
+	"github.com/mathisdev7/licensy-dashboard-backend/services"
 )
 
 func main() {
@@ -30,11 +31,24 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "https://licensy.xyz",
+		AllowOrigins: "https://licensy.xyz, http://localhost:3000",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+		AllowCredentials: true,
 	}))
 
 	app.Get("/api/users/@me/guilds", handlers.GetUserGuildsHandler(discord))
+	app.Get("/api/guilds/:guildID/roles", func(c *fiber.Ctx) error {
+		guildID := c.Params("guildID")
+		userID := c.Query("userID")
+		roleID := c.Query("roleID")
+
+		isRoleGreaterThan, err := services.IsRoleGreaterThan(botToken, guildID, userID, roleID)
+		if err != nil {
+			return err
+		}
+		return c.JSON(fiber.Map{"isRoleGreaterThan": !isRoleGreaterThan})
+	})
+
 	app.Get("/api/health", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{"status": "Server is running", "code": 200})
 	})
--- services/discord.go
+++ services/discord.go
@@ -10,12 +10,109 @@ import (
 )
 
 type Guild struct {
-	ID     string  `json:"id"`
-	Name   string  `json:"name"`
-	Icon   *string `json:"icon"`
-	Banner *string `json:"banner"`
-	Owner  bool    `json:"owner"`
-	Permissions string `json:"permissions"`
+	ID          string  `json:"id"`
+	Name        string  `json:"name"`
+	Icon        *string `json:"icon"`
+	Banner      *string `json:"banner"`
+	Owner       bool    `json:"owner"`
+	Permissions string  `json:"permissions"`
+}
+
+type Role struct {
+	ID       string `json:"id"`
+	Position int    `json:"position"`
+}
+
+func IsRoleGreaterThan(accessToken, guildID, userID, roleID string) (bool, error) {
+	userRoles, err := GetUserRolesInGuild(accessToken, guildID, userID)
+	if err != nil {
+		return false, err
+	}
+
+	roleToCompare, err := GetRoleInGuild(accessToken, guildID, roleID)
+	if err != nil {
+		return false, err
+	}
+
+	var highestRolePosition int
+	for _, userRole := range userRoles {
+		if userRole.Position > highestRolePosition {
+			highestRolePosition = userRole.Position
+		}
+	}
+
+	if roleToCompare.Position > highestRolePosition {
+		return true, nil
+	}
+	return false, nil
+}
+
+func GetUserRolesInGuild(accessToken, guildID, userID string) ([]Role, error) {
+	req, err := http.NewRequest("GET", "https://discord.com/api/v10/guilds/"+guildID+"/members/"+userID, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", "Bot "+ accessToken)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fiber.NewError(resp.StatusCode, "Failed to fetch user roles")
+	}
+
+	var member struct {
+		Roles []string `json:"roles"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&member); err != nil {
+		return nil, err
+	}
+
+	var roles []Role
+	for _, roleID := range member.Roles {
+		role, err := GetRoleInGuild(accessToken, guildID, roleID)
+		if err != nil {
+			return nil, err
+		}
+		roles = append(roles, role)
+	}
+
+	return roles, nil
+}
+
+func GetRoleInGuild(accessToken, guildID, roleID string) (Role, error) {
+	req, err := http.NewRequest("GET", "https://discord.com/api/v10/guilds/"+guildID+"/roles", nil)
+	if err != nil {
+		return Role{}, err
+	}
+	req.Header.Set("Authorization", "Bot "+accessToken)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return Role{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return Role{}, fiber.NewError(resp.StatusCode, "Failed to fetch roles in guild")
+	}
+
+	var roles []Role
+	if err := json.NewDecoder(resp.Body).Decode(&roles); err != nil {
+		return Role{}, err
+	}
+
+	for _, role := range roles {
+		if role.ID == roleID {
+			return role, nil
+		}
+	}
+
+	return Role{}, fiber.NewError(404, "Role not found")
 }
 
 func GetUserGuilds(accessToken string) ([]Guild, error) {
@@ -36,11 +133,11 @@ func GetUserGuilds(accessToken string) ([]Guild, error) {
 	}
 
 	var rawGuilds []struct {
-		ID    string  `json:"id"`
-		Name  string  `json:"name"`
-		Icon  *string `json:"icon"`
-		Owner bool    `json:"owner"`
-		Permissions string `json:"permissions"`
+		ID          string  `json:"id"`
+		Name        string  `json:"name"`
+		Icon        *string `json:"icon"`
+		Owner       bool    `json:"owner"`
+		Permissions string  `json:"permissions"`
 	}
 
 	if err := json.NewDecoder(resp.Body).Decode(&rawGuilds); err != nil {
@@ -50,11 +147,11 @@ func GetUserGuilds(accessToken string) ([]Guild, error) {
 	var userGuilds []Guild
 	for _, g := range rawGuilds {
 		userGuilds = append(userGuilds, Guild{
-			ID:     g.ID,
-			Name:   g.Name,
-			Icon:   utils.FormatIconURL(g.ID, g.Icon),
-			Banner: nil,
-			Owner:  g.Owner,
+			ID:          g.ID,
+			Name:        g.Name,
+			Icon:        utils.FormatIconURL(g.ID, g.Icon),
+			Banner:      nil,
+			Owner:       g.Owner,
 			Permissions: g.Permissions,
 		})
 	}
